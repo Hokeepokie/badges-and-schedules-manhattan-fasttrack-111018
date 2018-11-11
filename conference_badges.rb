d0d01e0caadd868ec70e9c4c4518b1cf51a6692f
Eliza Cheung <elizacheung7@gmail.com>
2018-11-11 20:28:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,17 +1,15 @@
-#method 
+speakers_array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+rooms_array = ["1", "2", "3", "4", "5", "6", "7"]
+
+
 def badge_maker (speaker_name) #putting the array name in the parentheses will display all the speakers' names in one output
   puts "Hello my name is #{speaker_name}"
 end
 
-#run the badge_maker method for each element in the array collection of speakers
-
-speakers_array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-
-
 def batch_badge_creator
 end
 
-rooms_array = ["1", "2", "3", "4", "5", "6", "7"]
 def assign_rooms
   #takes list of speakers_array[], assigns to room
 end 
