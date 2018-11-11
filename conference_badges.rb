f5cb6f988c1d061f07ceeb60cc33b5ce7d0e35c9
Eliza Cheung <elizacheung7@gmail.com>
2018-11-11 20:18:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-puts "hi"
+#method 
+def badge_maker (speaker_name)
+  puts "Hello my name is #{speaker_name}"
+end
+
+#run the badge_maker method for each element in the array collection of speakers
+
+speakers_array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+puts speakers_array
