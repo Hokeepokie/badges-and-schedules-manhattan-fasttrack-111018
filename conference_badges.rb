39226dc67566dbc12391fd84246934a12608793f
Eliza Cheung <elizacheung7@gmail.com>
2018-11-11 20:21:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 #method 
-def badge_maker (speaker_name)
+def badge_maker (speaker_name) #putting the array name in the parentheses will display all the speakers' names in one output
   puts "Hello my name is #{speaker_name}"
 end
 
@@ -7,4 +7,3 @@ end
 
 speakers_array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
-badge_maker (speakers_array)
