97a3bd6bfd6c8a676a700c8733d1483da9fcde9a
Eliza Cheung <elizacheung7@gmail.com>
2018-11-11 20:36:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,10 +7,10 @@ def badge_maker (speaker_name) #putting the array name in the parentheses will d
   puts "Hello my name is #{speaker_name}"
 end
 
-def batch_badge_creator (Array)
+def batch_badge_creator (r)
   counter =0
   while counter <= speakers_array.length 
-  badge_maker (Array)
+  badge_maker (r)
   counter =+ 1 
 end
 
